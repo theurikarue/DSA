9f07171cc38dd08b680348ae999467a5dfb6e64e
Bonitheuri <133132495+theurikarue@users.noreply.github.com>
2023-05-22 20:27:17 +0300

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int checkbalance(string code){
+stack <char> stack;
+for(int i=0;i<code.length();i++){
+char c=code[i];
+if(c=='('||c=='{'){
+stack.push(c);
+}
+  else if(c==')'){
+   if(stack.top()=='(')
+       stack.pop();
+    else 
+      return i;
+}
+else if(c=='}'){
+   if(stack.top()=='{')
+       stack.pop();
+    else 
+      return i;
+  }
+ }
+return 0;
+}
+
+int main(){
+string code= "int main(){string code=""result= chackbalance(code);cout<<stack.top();}";
+int result = checkbalance(code);//function call
+cout<<"the result of the call:"<<result;
+return 0;
+}
