444ceb15cc209040c1a1efc760688562bde61dcd
Bonitheuri <133132495+theurikarue@users.noreply.github.com>
2023-05-24 04:15:28 -0700

    Update shell sort.cpp

--- shell sort.cpp
+++ shell sort.cpp
@@ -1,20 +1,20 @@
-// C++ implementation of Shell Sort
+/* C++ implementation of Shell Sort*/
 #include  <iostream>
 using namespace std;
   
 /* function to sort arr using shellSort */
 int shellSort(int arr[], int n)
 {
-    // Start with a big gap, then reduce the gap
+    /* Start with a big gap, then reduce the gap*/
     for (int gap = n/2; gap > 0; gap /= 2)
     {
-        // Do a gapped insertion sort for this gap size.The first gap elements a[0..gap-1] are already in gapped order keep adding one more element until the entire array is gap sorted 
-        for (int i = gap; i < n; i += 1)
+        /* Do a gapped insertion sort for this gap size.The first gap elements a[0..gap-1] are already in gapped order keep adding one more element until the entire array is gap sorted 
+        */for (int i = gap; i < n; i += 1)
         {
-            // add a[i] to the elements that have been gap sorted save a[i] in temp and make a hole at position i
+            /* add a[i] to the elements that have been gap sorted save a[i] in temp and make a hole at position i */
             int temp = arr[i];
   
-            // shift earlier gap-sorted elements up until the correct location for a[i] is found
+            /*shift earlier gap-sorted elements up until the correct location for a[i] is found*/
             int j;            
             for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
                 arr[j] = arr[j - gap];
